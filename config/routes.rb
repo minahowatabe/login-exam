ae2dd00073eea1933f3b3d21005503f01698e5df
minaho_watabe <minahohk@hotmail.com>
2018-05-07 02:03:39 +0000

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'blogs#top'
-  
   resources :blogs do
    collection do
      post :confirm
    end   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
+
+root to:'blogs#top'  
 end
+
